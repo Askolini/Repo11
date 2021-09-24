03c75bdb5850f49f86ca3eaf2bac756770aee520
Askolini <asmircc@gmail.com>
2021-09-24 03:58:41 +0200

    Falsy Ej

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function myBouncer(arr) {
+    var array = [
+        [7, "ate", "", false, 9],
+        ["a", "b", "c"],
+        [false, null, 0, NaN, undefined, ""],
+        [null, Nan, 1, 2, undefined]
+    ];
+
+    return
+}
+
+console.log(myBouncer());
+module.exports = myBouncer;
