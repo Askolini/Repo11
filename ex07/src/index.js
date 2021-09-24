e69ba7d707739d49e5e1bf9109667658686392f1
Askolini <asmircc@gmail.com>
2021-09-24 03:39:11 +0200

    Return Largest Num

---
+++ ex07/src/index.js
@@ -0,0 +1,22 @@
+function largestNumFromArr(arr) {
+    maxNumArray = [];
+
+}
+
+console.log(largestNumFromArr());
+module.exports = largestNumFromArr;
+
+[13, 27, 18, 26],
+[4, 5, 1, 3],
+[32, 35, 37, 39],
+[1000, 1001, 857, 1]
+
+[4, 9, 1, 3],
+[13, 35, 18, 26],
+[32, 35, 97, 39],
+[1000000, 1001, 857, 1]
+
+[17, 23, 25, 12],
+[25, 7, 34, 48],
+[4, 10, 18, 21],
+[72, 3, 17, 10]
