c3af44df9c42b279eae448ce7a861636e4e3cdde
Askolini <asmircc@gmail.com>
2021-09-24 04:06:10 +0200

    Spl

---
+++ ex10/src/index.js
@@ -0,0 +1,23 @@
+function mySplice(arr1, arr2, n) {
+    var arr1 =
+    var arr2 = 
+    var n =
+    return arr2;
+}
+
+[1, 2, 3],
+[4, 5],
+1
+
+[1, 2, 3],
+[4, 5],
+2
+
+["b", "c"],
+["a", "d"],
+1
+
+["Dell", "Philips"], 
+["Samsung", "AOX", "Sony"],
+2
+
