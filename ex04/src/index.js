334ae99cbc14a58816c628f9647d76ba08950a1a
Askolini <asmircc@gmail.com>
2021-09-24 02:41:04 +0200

    Store Multiple Val

---
+++ ex04/src/index.js
@@ -0,0 +1,11 @@
+var monitorsListArray = ["Apple", "Peach", "Berry"];
+
+function myMonitorsFunction(arr) {
+    var newMonitorsList = [...arr];
+    var monitorsList = [];
+    for ( var monitorsList = 0; monitorsList < 4; monitorsList++);
+    return newMonitorsList;
+}
+
+console.log(myMonitorsFunction(monitorsListArray));
+module.exports = myMonitorsFunction;
