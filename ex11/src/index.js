f3ddc60e1d049561507e952a9c96dad42833990a
Askolini <asmircc@gmail.com>
2021-09-24 04:14:42 +0200

    Split Arr

---
+++ ex11/src/index.js
@@ -0,0 +1,21 @@
+function splitArrayInGroups(arr, n) {
+    result = [];
+}
+
+console.log(splitArrayInGroups());
+module.exports = splitArrayInGroups;
+
+["a", "b", "c", "d"]
+2
+
+[0, 1, 2, 3, 4, 5, 6]
+3
+
+[0, 1, 2, 3, 4, 5]
+2
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8]
+3
+
+[0, 1, 2, 3, 4, 5, 6, 7, 8]
+2
