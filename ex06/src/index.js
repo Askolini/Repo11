75f56610934e7afed27da63e69226a4ed766b862
Askolini <asmircc@gmail.com>
2021-09-24 03:18:22 +0200

    Nesting for L

---
+++ ex06/src/index.js
@@ -0,0 +1,25 @@
+function multiplyArrayFunction(arr) {
+    var myArray = [
+        [
+            [2],
+            [5, 6, 7],
+            [8, 9]
+        ]
+        [
+            [2.5, 2],
+            [0.5, 0.2],
+            [8]
+        ]
+        [
+            [1, 2],
+            [3, 4, 5, 6],
+            [7, 8]
+        ]
+    ]
+    var product = [0[0]*[1]*[2]];
+    var sum = [0[0]+[1]+[2]];
+    return myArray;
+}
+
+console.log(multiplyArrayFunction(myArray));
+module.exports = multiplyArrayFunction;
