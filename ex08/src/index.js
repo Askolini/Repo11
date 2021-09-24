cee718f5069309c071c5409214b9736d025c40f2
Askolini <asmircc@gmail.com>
2021-09-24 03:51:14 +0200

    String Mutat

---
+++ ex08/src/index.js
@@ -0,0 +1,18 @@
+function myMutation(arr) {
+
+}
+
+console.log(myMutation());
+module.exports = myMutation;
+
+["hello", "hey"],
+["hello", "Hello"],
+["zyxwvutsrqponmlkjihgfedcba", "qrstu"],
+["Mary", "Army"],
+["Alien", "line"],
+["floor", "for"],
+["hello", "neo"],
+["voodo", "no"],
+["ate", "date"],
+["Tiger", "Zebra"],
+["Noel", "Ole"]
